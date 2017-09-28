df0bdcfd9067d34202ab7fd641762a4d25ef4a74
Matt Dulany <matt_dulany@baylor.edu>
2017-09-28 11:36:47 -0500

    Attempt to fix 403 error

--- src/main/java/petfinder/site/WebSecurityConfiguration.java
+++ src/main/java/petfinder/site/WebSecurityConfiguration.java
@@ -1,11 +1,17 @@
 package petfinder.site;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /**
  * Created by jlutteringer on 8/22/17.
@@ -15,8 +21,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		/*http
-				.authorizeRequests()
+		http
+			.cors().and();
+				/*.authorizeRequests()
 					.antMatchers("/").permitAll()
 					.antMatchers("/statics/**").permitAll()
 				.anyRequest().authenticated()
@@ -27,6 +34,17 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 					.and()
 				.logout()
 					.permitAll();*/
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("https://tempeturs-group-2.herokuapp.com"));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 
 	@Autowired
