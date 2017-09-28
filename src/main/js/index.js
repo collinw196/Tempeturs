2b9550b01203da328448b17062ee67462afd0f1e
Matt Dulany <matt_dulany@baylor.edu>
2017-09-28 16:14:37 -0500

    quick fixes to login page stuff

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 
-import { Home, Page1, Page2, Page3 } from 'js/pages';
+import { Home, Login, Page1, Page2, Page3 } from 'js/pages';
 
 export default class Index extends React.Component {
 	render() {
@@ -9,6 +9,7 @@ export default class Index extends React.Component {
 			<HashRouter>
 				<div>
 					<Route exact path="/" component={Home} />
+					<Route exact path="/login" component={Login} />
 					<Route exact path="/page-1" component={Page1} />
 					<Route exact path="/page-2" component={Page2} />
 					<Route exact path="/page-3" component={Page3} />
--- src/main/js/pages.js
+++ src/main/js/pages.js
@@ -34,7 +34,7 @@ export class Home extends React.Component {
         <div className="container padded">
             <h1>This is the home page.</h1>
 	    <ul>
-		<li><Link to="/log-in">Log-in</Link></li>
+		<li><Link to="/login">Login</Link></li>
 	    </ul>
             <p>{this.state.message}</p>
         </div>
@@ -42,7 +42,7 @@ export class Home extends React.Component {
     }
 }
 
-export class Log-in extends React.Component {
+export class Login extends React.Component {
 	constructor(props) {
 	    super(props);
 	    this.state = {
@@ -68,6 +68,8 @@ export class Log-in extends React.Component {
     	event.preventDefault();
     	const {username,
     			password} = this.state;
+    	this.props.history.push('/');
+    }
     	
 	
 	render() {
@@ -82,6 +84,64 @@ export class Log-in extends React.Component {
 						<input type="text" name="password" /><br />
 					</form>
 				</div>
+			</div>
+		);
+	}
+}
+
+export class Page1 extends React.Component {
+	render() {
+		return (
+			<div className="container padded">
+				<div>
+					<h2>Registration</h2>
+					<form>
+						First Name:<br />
+						<input type="text" name="firstname" /><br />
+						Last name:<br />
+						<input type="text" name="lastname" /><br />
+						Email:<br />
+						<input type="text" name="email" /><br />
+						Address:<br />
+						<br />
+						Street Number:<br />
+						<input type="text" name="street" /><br />
+						*Street Address 2:<br />
+						<input type="text" name="street" /><br />
+						*PO Box:<br />
+						<input type="text" name="PO Box" /><br />
+						ZIP Code:<br />
+						<input type="number" name="zip" /><br />
+						State:<br />
+						<input type="text" name="state" /><br />
+						Phone Number:<br />
+						<input type="number" name="number" /><br />
+						<input type="radio" name="gender" value="male" checked /> Male<br />
+  						<input type="radio" name="gender" value="female" /> Female<br />
+  						<input type="radio" name="type" value="Pet Owner Account" checked /> Pet Owner Account<br />
+  						<input type="radio" name="type" value="Pet Sitter Account" /> Pet Sitter Account
+					</form>
+				</div>
+			</div>
+		);
+	}
+}
+
+export class Page2 extends React.Component {
+	render() {
+		return (
+			<div className="container padded">
+				This is page 2.
+			</div>
+		);
+	}
+}
+
+export class Page3 extends React.Component {
+	render() {
+		return (
+			<div className="container padded">
+				This is page 3.
 			</div>
 		);
 	}
