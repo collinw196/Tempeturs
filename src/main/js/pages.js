e0704403489035d787fce485ed33df5408f2c0f8
Collin Wiginton <collinw196@gmail.com>
2017-09-28 21:32:28 +0000

    Update pages.js

--- src/main/js/pages.js
+++ src/main/js/pages.js
@@ -82,6 +82,7 @@ export class Login extends React.Component {
 						<input type="text" name="username" /><br />
 						Password:<br />
 						<input type="text" name="password" /><br />
+						<input type="submit" name="Submit" /><br />
 					</form>
 				</div>
 			</div>
