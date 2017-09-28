4bf1b171e8e6fad15b80ad62eac09786cbef4f70
Collin Wiginton <collinw196@gmail.com>
2017-09-28 20:51:35 +0000

    Update pages.js

--- src/main/js/pages.js
+++ src/main/js/pages.js
@@ -34,9 +34,7 @@ export class Home extends React.Component {
         <div className="container padded">
             <h1>This is the home page.</h1>
 	    <ul>
-		<li><Link to="/page-1">Page 1</Link></li>
-		<li><Link to="/page-2">Page 2</Link></li>
-		<li><Link to="/page-3">Page 3</Link></li>
+		<li><Link to="/log-in">Log-in</Link></li>
 	    </ul>
             <p>{this.state.message}</p>
         </div>
@@ -44,67 +42,46 @@ export class Home extends React.Component {
     }
 }
 
-export class Page1 extends React.Component {
+export class Log-in extends React.Component {
+	constructor(props) {
+	    super(props);
+	    this.state = {
+	    	username:'',
+	    	password: ''    	
+	    };
+	
+	    this.handleChange = this.handleChange.bind(this);
+	    this.handleSubmit = this.handleSubmit.bind(this);
+    }
+	
+    handleChange(event) {
+	    const target = event.target;
+	    const value = target.value;
+	    const name = target.name;
+	
+	    this.setState({
+	      [name]: value
+	    });
+	}
+	
+    handleSubmit(event) {
+    	event.preventDefault();
+    	const {username,
+    			password} = this.state;
+    	
+	
 	render() {
 		return (
 			<div className="container padded">
 				<div>
-					<h2>Registration</h2>
+					<h2>Sign in</h2>
 					<form>
-						First Name:<br />
-						<input type="text" name="firstname" /><br />
-						Last name:<br />
-						<input type="text" name="lastname" /><br />
-						Email:<br />
-						<input type="text" name="email" /><br />
-						Address:<br />
-						<br />
-						Street Number:<br />
-						<input type="text" name="street" /><br />
-						*Street Address 2:<br />
-						<input type="text" name="street" /><br />
-						*PO Box:<br />
-						<input type="text" name="PO Box" /><br />
-						ZIP Code:<br />
-						<input type="number" name="zip" /><br />
-						State:<br />
-						<input type="text" name="state" /><br />
-						Phone Number:<br />
-						<input type="number" name="number" /><br />
-						<input type="radio" name="gender" value="male" checked /> Male<br />
-  						<input type="radio" name="gender" value="female" /> Female<br />
-  						<input type="radio" name="type" value="Pet Owner Account" checked /> Pet Owner Account<br />
-  						<input type="radio" name="type" value="Pet Sitter Account" /> Pet Sitter Account
+						Username:<br />
+						<input type="text" name="username" /><br />
+						Password:<br />
+						<input type="text" name="password" /><br />
 					</form>
 				</div>
-			</div>
-		);
-	}
-}
-
-export class Page2 extends React.Component {
-	render() {
-		return (
-			<div className="container padded">
-				<div>
-				    <h2>User Sign-in</h2>
-				    <form>
-				        Username: <br />
-				        <input type="text" name="username" /><br />
-				        Password: <br />
-				        <input type="text" name="password" /><br />
-				    </form>
-				</div>
-			</div>
-		);
-	}
-}
-
-export class Page3 extends React.Component {
-	render() {
-		return (
-			<div className="container padded">
-				This is page 3.
 			</div>
 		);
 	}
