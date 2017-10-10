655d78b79e51f019b3ab85082c81f15120a5f7a3
Collin Wiginton <collinw196@gmail.com>
2017-10-10 18:29:30 +0000

    Update pages.js

--- src/main/js/pages.js
+++ src/main/js/pages.js
@@ -1,6 +1,29 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import PropTypes from 'prop-types';
+import React from 'react';
+import uncontrollable from 'uncontrollable';
+import cn from 'classnames';
+import {
+  accessor
+  , elementType
+  , dateFormat
+  , dateRangeFormat
+  , views as componentViews
+} from './utils/propTypes';
+
+import { notify } from './utils/helpers';
+import { navigate, views } from './utils/constants';
+import defaultFormats from './formats';
+import message from './utils/messages';
+import moveDate from './utils/move';
+import VIEWS from './Views';
+import Toolbar from './Toolbar';
+import EventWrapper from './EventWrapper';
+import BackgroundWrapper from './BackgroundWrapper';
+
+import omit from 'lodash/omit';
+import defaults from 'lodash/defaults';
+import transform from 'lodash/transform';
+import mapValues from 'lodash/mapValues';
 
 
 export class Home extends React.Component {
