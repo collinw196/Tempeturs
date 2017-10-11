6dab6f6331a7ef5caf3c99156c3ef8a73bc87306
Matt Dulany <matt_dulany@baylor.edu>
2017-10-11 14:47:58 -0500

    Fixes part2

--- src/main/java/petfinder/site/common/owner/OwnerDao.java
+++ src/main/java/petfinder/site/common/owner/OwnerDao.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 public class OwnerDao {
+	@Autowired
 	private OwnerDto owner;
 	
 	public OwnerDto findOwner(Long id) {
