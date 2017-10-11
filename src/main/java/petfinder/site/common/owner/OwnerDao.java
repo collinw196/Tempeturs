a2f994c49d09522fe60697234faca3214fa4eb26
Matt Dulany <matt_dulany@baylor.edu>
2017-10-11 14:55:45 -0500

    Matt is really dumb

--- src/main/java/petfinder/site/common/owner/OwnerDao.java
+++ src/main/java/petfinder/site/common/owner/OwnerDao.java
@@ -1,10 +1,11 @@
 package petfinder.site.common.owner;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+@Repository
 public class OwnerDao {
-	@Autowired
 	private OwnerDto owner;
 	
 	public OwnerDto findOwner(Long id) {
