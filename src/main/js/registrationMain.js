35c6cc202685298fff62762260713bbe5af0d277
Matt Dulany <matt_dulany@baylor.edu>
2017-10-16 19:19:42 -0500

    Adding more files to split the front end for clarity

---
+++ src/main/js/registrationMain.js
@@ -0,0 +1,140 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.put['Content-Type'] = 'application/json';
+
+
+export class Registration extends React.Component {
+	constructor(props) {
+	    super(props);
+	    this.state = {
+	    	firstName: '',
+	    	lastName: '',
+	    	email: '',
+	    	username: '',
+	    	password: '',
+	    	repassword: '',
+	    	street1: '',
+	    	street2: '',
+	    	po: '',
+	    	zip: '',
+	    	state: '',
+	    	phone: '',
+	    	gender: '',
+	    	type: ''
+	    };
+	
+	    this.handleChange = this.handleChange.bind(this);
+	    this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+     handleChange(event) {
+	    const target = event.target;
+	    const value = target.value;
+	    const name = target.name;
+	
+	    this.setState({
+	      [name]: value
+	    });
+	}
+	
+    handleSubmit(event) {
+    	event.preventDefault();
+    	const {firstName,
+	    	lastName,
+	    	email,
+	    	username,
+	    	password,
+	    	repassword,
+	    	street1,
+	    	street2,
+	    	po,
+	    	zip,
+	    	state,
+	    	phone,
+	    	gender,
+	    	type} = this.state;
+	    	
+	    axios.post('https://tempeturs-group-2.herokuapp.com/api/user/reg', {withCredentials:true}, {
+		    firstName,
+	    	lastName,
+	    	email,
+	    	username,
+	    	password,
+	    	street1,
+	    	street2,
+	    	po,
+	    	zip,
+	    	state,
+	    	phone,
+	    	gender,
+	    	type
+		  })
+		  .then(function (response) {
+		    console.log(response);
+		  })
+		  .catch(function (error) {
+		    console.log(error);
+		  });
+		  
+    	if (this.state.type == 'owner') {
+    		this.props.history.push('/reg/owner');
+    	}
+    	else {
+    		this.props.history.push('/reg/sitter');
+    	}
+    }
+    
+    render() {
+		return (
+			<div className="container padded">
+				<div>
+					<h2>Registration</h2>
+					<form onSubmit={this.handleSubmit}>
+						First Name:<br />
+						<input name="firstName" type="text" value={this.state.firstName} onChange={this.handleChange} required /><br />
+						Last name:<br />
+						<input name="lastName" type="text" value={this.state.lastName} onChange={this.handleChange} required /><br />
+						Email:<br />
+						<input name="email" type="text" value={this.state.email} onChange={this.handleChange} required pattern=".*@.*\..*"/><br />
+						Username:<br />
+						<input name="username" type="text" value={this.state.username} onChange={this.handleChange} required /><br />
+						Password:<br />
+						<input name="password" type="password" value={this.state.password} onChange={this.handleChange} required /><br />
+						Reenter Password:<br />
+						<input name="repassword" type="password" value={this.state.repassword} onChange={this.handleChange} required /><br />
+						
+						
+						Address:<br />
+						<br />
+						Street Number:<br />
+						<input name="street1" type="text" value={this.state.street1} onChange={this.handleChange} required/><br />
+						*Street Address 2:<br />
+						<input name="street2" type="text" value={this.state.street2} onChange={this.handleChange} /><br />
+						*PO Box:<br />
+						<input name="po" type="text" value={this.state.po} onChange={this.handleChange} /><br />
+						ZIP Code:<br />
+						<input name="zip" type="text" value={this.state.zip} onChange={this.handleChange} required pattern="[0-9]{5}"/><br />
+						State:<br />
+						<input name="state" type="text" value={this.state.state} onChange={this.handleChange} required/><br />
+						Phone Number:<br />
+						<input name="phone" type="text" value={this.state.phone} onChange={this.handleChange} required pattern="[0-9]{10} | [0-9]{3}-[0-9]{3}-[0-9]{4}" /><br />
+						
+						<br />
+						Gender: <br />
+						<input type="radio" name="gender" value="male" onChange={this.handleChange} required/> Male<br />
+  						<input type="radio" name="gender" value="female" onChange={this.handleChange} /> Female<br />
+  						<br />
+						Account Type (you may have one type of each account per email): <br />
+  						<input type="radio" name="type" value="owner" onChange={this.handleChange} required/> Pet Owner Account<br />
+  						<input type="radio" name="type" value="sitter" onChange={this.handleChange} /> Pet Sitter Account<br />
+  						<input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/> 
+  						<input type="submit" value="Submit" />
+  					</form>
+				</div>
+			</div>
+		);
+	}
+}
---
+++ src/main/js/sitterRegistration.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.put['Content-Type'] = 'application/json';
+
+export class SitterInfo extends React.Component {
+	constructor(props) {
+	    super(props);
+	    this.state = {
+	    	accnumber: '',
+	    	rounumber: '',
+	    	pettype1: '',
+	    	pettype2: '',
+	    	pettype3: ''	    	
+	    };
+	
+	    this.handleChange = this.handleChange.bind(this);
+	    this.handleSubmit = this.handleSubmit.bind(this);
+    }
+	
+    handleChange(event) {
+	    const target = event.target;
+	    const value = target.value;
+	    const name = target.name;
+	
+	    this.setState({
+	      [name]: value
+	    });
+	}
+	
+    handleSubmit(event) {
+    	event.preventDefault();
+    	const {accnumber,
+    		rounumber,
+    		pettype1,
+    		pettype2,
+    		pettype3} = this.state;
+    	
+    	axios.post('https://tempeturs-group-2.herokuapp.com/api/sitter/reg', {withCredentials:true}, {
+		    accnumber,
+    		rounumber,
+    		pettype1,
+    		pettype2,
+    		pettype3
+		  })
+		  .then(function (response) {
+		    console.log(response);
+		  })
+		  .catch(function (error) {
+		    console.log(error);
+		  });
+		  
+		  
+    	this.props.history.push('/');
+    }
+	render() {
+		return (
+			<div className="container padded">
+				<div>
+					<h5>Pet Sitter Information</h5>
+					<form onSubmit={this.handleSubmit}>
+						Payment Account Number:<br />
+						<input name="accnumber" type="number" value={this.state.accnumber} onChange={this.handleChange} required /><br />
+						Payment Routing Number:<br />
+						<input name="rounumber" type="number" value={this.state.rounumber} onChange={this.handleChange} required /><br />
+						Pet Preference 1:<br />
+						<select name="pettype1" onChange={this.handleChange} required>
+							<option value="dog" selected>Dog</option>
+							<option value="cat" selected>Cat</option>
+							<option value="horse" selected>Horse</option>
+							<option value="ferret" selected>Ferret</option>
+							<option value="rabbit" selected>Rabbit</option>
+							<option value="fish" selected>Fish</option>
+						</select><br />
+						Pet Preference 2:<br />
+						<select name="pettype2" onChange={this.handleChange} required>
+							<option value="dog" selected>Dog</option>
+							<option value="cat" selected>Cat</option>
+							<option value="horse" selected>Horse</option>
+							<option value="ferret" selected>Ferret</option>
+							<option value="rabbit" selected>Rabbit</option>
+							<option value="fish" selected>Fish</option>
+						</select><br />
+						Pet Preference 3:<br />
+						<select name="pettype3" onChange={this.handleChange} required>
+							<option value="dog" selected>Dog</option>
+							<option value="cat" selected>Cat</option>
+							<option value="horse" selected>Horse</option>
+							<option value="ferret" selected>Ferret</option>
+							<option value="rabbit" selected>Rabbit</option>
+							<option value="fish" selected>Fish</option>
+						</select><br />
+						<input type="submit" value="Submit" />
+					</form>
+				</div>
+				
+			</div>
+		);
+	}
+}
