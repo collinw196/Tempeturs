814fab871b8924995ff4ab13188f6edf1c07b821
Matt Dulany <matt_dulany@baylor.edu>
2017-11-01 17:21:46 -0500

    Fix password issue

--- src/main/js/homePages.js
+++ src/main/js/homePages.js
@@ -106,7 +106,7 @@ export class Login extends React.Component {
 						Username:<br />
 						<input type="text" name="username" /><br />
 						Password:<br />
-						<input type="text" name="password" /><br />
+						<input type="password" name="password" /><br />
 						<input type="radio" name="type" value="owner" onChange={this.handleChange} required/> Pet Owner Account<br />
   						<input type="radio" name="type" value="sitter" onChange={this.handleChange} /> Pet Sitter Account<br />
 						<input type="submit" value="Submit" /><br />
