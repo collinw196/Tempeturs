ba2c254c8839d1d0432414562453fb3efa8bec7f
Marcus Hollingsworth <marcus_hollingsworth@baylor.edu>
2017-11-03 07:47:51 +0000

    Update notifications.js

--- src/main/js/notifications.js
+++ src/main/js/notifications.js
@@ -16,11 +16,13 @@ export class Notifications extends React.Component {
 				<div>
 					<h5>Notifications</h5>
 					<div>
-						//<ul>
-						//	<li><Link to="/owner/reserve">Create Appointment</Link></li>
-						//	<li><Link to="/owner/appoint">Current Appointments</Link></li>
-						//	<li><Link to="/owner/pets">PetInfo</Link></li>
-						//</ul>
+						<ul>
+							<li>Notification 1</li>
+							<li>Notification 2</li>
+							<li>Notification 3</li>
+							<li>Notification 4</li>
+							<li>Notification 5</li>
+						</ul>
 					</div>
 				</div>
 			</div>
