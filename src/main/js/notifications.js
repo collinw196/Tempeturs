0bc16420c540b8c31acb452d831f14f1d9eaec1c
Marcus Hollingsworth <marcus_hollingsworth@baylor.edu>
2017-11-07 21:35:21 +0000

    Add link to notifications from sitter homepage

--- src/main/js/notifications.js
+++ src/main/js/notifications.js
@@ -5,6 +5,30 @@ import axios from 'axios';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.put['Content-Type'] = 'application/json';
 
+
+export class SitterHome extends React.Component {
+	constructor(props) {
+	    super(props);
+    }
+    
+	render() {
+		return (
+			<div className="container padded">
+				<div>
+					<h5>Pet Sitter Home Page</h5>
+					<ul>
+						<li><Link to="/sitter/calendar">Schedule</Link></li>
+						<li><Link to="/sitter/notifications">Notifications</Link></li>
+					</ul>
+					
+				</div>
+				
+			</div>
+		);
+	}
+}
+
+
 export class Notifications extends React.Component {
 	constructor(props) {
 	    super(props);
