3d8498681f340c72b6a54afce2b9e0e27ea0c5f5
Collin Wiginton <collinw196@gmail.com>
2017-11-07 22:07:22 +0000

    Update sitterPages.js

--- src/main/js/sitterPages.js
+++ src/main/js/sitterPages.js
@@ -43,16 +43,7 @@ export class WeekView extends React.Component{
                 		<td width="100">
                 		    <tr>
                 		        <script>
-                                var now = new Date();
-                                var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
-                                var date = ((now.getDate() < 10) ? "0" : "") + now.getDate();
-                                function fourdigits(number) {
-                                    return (number < 1000) ? number + 1900 : number;
-                                }
-                                today =  months[now.getMonth()] + " " +
-                                    date + ", " +
-                                    (fourdigits(now.getYear()));
-                                    document.write(today);
+                                    document.write("11/7/2017");
                                 </script>
                             </tr>
                 		    <tr></tr>
