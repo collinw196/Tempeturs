5ca3f7cdaf834b67740f33c0db52614540cc90be
Matt Dulany <matt_dulany@baylor.edu>
2017-11-08 22:26:20 -0600

    Working on CUDS

--- src/main/java/petfinder/site/CustomUserDetailService.java
+++ src/main/java/petfinder/site/CustomUserDetailService.java
@@ -43,7 +43,6 @@ public class CustomUserDetailService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		System.out.println("\n\n Stop -3 \n\n");
 		System.out.println(username);
 		SearchRequest searchRequest = new SearchRequest("users"); 
 		searchRequest.types("external");
@@ -52,17 +51,18 @@ public class CustomUserDetailService implements UserDetailsService {
 		sourceBuilder.query(matchQueryBuilder); 
 		searchRequest.source(sourceBuilder);
 		SearchResponse response = null;
-		System.out.println("\n\n Stop -2 \n\n");
 		try {
+			System.out.println("\n\n Stop -3 \n\n");
 			response = clientService.getHighClient().search(searchRequest);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		System.out.println("\n\n Stop -2 \n\n");
 		if(response.getHits().getTotalHits() == 0){
 			throw new BadCredentialsException("Username and password not recognized");
 		}
-
+		System.out.println("\n\n Stop -1.5 \n\n");
 		JSONObject json = new JSONObject(response.toString());
 		JSONObject firstHits = json.getJSONObject("hits");
 		JSONArray secondHits = firstHits.getJSONArray("hits");
@@ -71,7 +71,7 @@ public class CustomUserDetailService implements UserDetailsService {
 		String jsonString = source.toString();
 		UserDto user = null;
 		
-		
+		System.out.println("\n\n Stop -1 \n\n");
 		try {
 			user = objectMapper.readValue(jsonString, UserDto.class);
 		} catch (IOException e) {
@@ -80,8 +80,6 @@ public class CustomUserDetailService implements UserDetailsService {
 		}
         String role = user.getRole();
         
-        System.out.println("\n\n Stop -1 \n\n");
- 
         List<SimpleGrantedAuthority> authList = getAuthorities(role);
         System.out.println("\n\n Stop 0 \n\n");
         
