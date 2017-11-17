22a2a1b85f946acac2d2807470d3e5c1439000ca
Marcus Hollingsworth <marcus_hollingsworth@baylor.edu>
2017-11-17 03:32:58 +0000

    Backend connection to notifications

--- src/main/js/notifications.js
+++ src/main/js/notifications.js
@@ -6,33 +6,61 @@ axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.put['Content-Type'] = 'application/json';
 
 
-export class SitterHome extends React.Component {
-	constructor(props) {
-	    super(props);
-    }
-    
-	render() {
-		return (
-			<div className="container padded">
-				<div>
-					<h5>Pet Sitter Home Page</h5>
-					<ul>
-						<li><Link to="/sitter/calendar">Schedule</Link></li>
-						<li><Link to="/sitter/notifications">Notifications</Link></li>
-					</ul>
-					
-				</div>
-				
-			</div>
-		);
-	}
-}
-
-
 export class Notifications extends React.Component {
 	constructor(props) {
 	    super(props);
+	    this.state = {
+	        apptNotifications: []
+	        ratNotifications: []
+	    };
+	    this.componentDidMount() = this.componentDidMount.bind(this);
+	    this.displayNotifications() = this.displayNotifications.bind(this);
     }
+    
+    componentDidMount() {
+        axios.get('http://tempeturs-group-2.herokuapp.com/api/sitter/notifications/appt')
+            .then(data => {
+            this.setState({apptNotifications: data.data});
+            })
+        .catch(function(error) {
+            console.log(error);
+        });
+        
+        axios.get('http://tempeturs-group-2.herokuapp.com/api/sitter/notifications/rat')
+            .then(data => {
+            this.setState({ratNotifications: data.data});
+            })
+        .catch(function(error) {
+            console.log(error);
+        });
+    }
+    
+    displayNotifications() {
+        for(var i = 0; i < this.state.apptNotifications.length; i++) {
+            var fragment = create('<li>New appointment request from ' + 
+                this.state.apptNotifications[i].getOwnerUsername() + '.</li>');
+        }
+        
+        for(var i = 0; i < this.state.ratNotifications.length; i++) {
+            var fragment = create('<li>' + this.state.ratNotifications[i] + '</li>');
+        }
+    }
+   
+   /////// 
+    create(htmlStr) {
+        var frag = document.createDocumentFragment(),
+            temp = document.createElement('div');
+        temp.innerHTML = htmlStr;
+        while (temp.firstChild) {
+            frag.appendChild(temp.firstChild);
+        }
+        return frag;
+    }
+
+// You can use native DOM methods to insert the fragment:
+document.body.insertBefore(fragment, document.body.childNodes[0]);
+    ////////
+    
     
 	render() {
 		return (
