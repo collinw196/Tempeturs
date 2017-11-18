bd4dfcb15e6a09b059ce8b7358e8b6ef024f226d
Collin Wiginton <Collin Wiginton@10.62.37.12>
2017-11-18 12:27:43 -0600

    test

--- src/main/js/sitterPages.js
+++ src/main/js/sitterPages.js
@@ -18,6 +18,7 @@ export class SitterHome extends React.Component {
 					<h5>Pet Sitter Home Page</h5>
 					<ul>
 						<li><Link to="/sitter/calendar">Schedule</Link></li>
+						<li><Link to="/sitter/appointmentInfo">Appt</Link></li>
 					</ul>
 					
 				</div>
