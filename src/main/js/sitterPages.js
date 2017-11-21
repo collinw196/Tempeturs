6faecc16b969b1c985dca2e7148be5d73e74f079
Collin Wiginton <Collin Wiginton@10.63.29.6>
2017-11-21 13:08:03 -0600

    efwef

--- src/main/js/sitterPages.js
+++ src/main/js/sitterPages.js
@@ -56,7 +56,7 @@ export class WeekView extends React.Component{
 	    
         for (var i = 0; i < this.state.week.length; i++) {
         	for(var j = 0; j < this.state.week[i].length; j++) {
-        		this.state.week[i][j] = new Object(var username = '', blockId = '', startHour = '', startMin = '', endHour = '', endMin = '');
+        		this.state.week[i][j] = new Object(this.username = '', blockId = '', startHour = '', startMin = '', endHour = '', endMin = '');
         	}
         }
         this.getDateHeader = this.getDateHeader.bind(this);
