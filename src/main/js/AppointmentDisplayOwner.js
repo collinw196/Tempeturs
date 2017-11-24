440f083440973abc7e475dbbfbbff13a49ff2faa
Boijangle <hitechdill@gmail.com>
2017-11-24 16:04:18 -0600

    Pets list and cancel appt?

--- src/main/js/AppointmentDisplayOwner.js
+++ src/main/js/AppointmentDisplayOwner.js
@@ -11,7 +11,8 @@ export class OwnerApptDisplay extends React.Component {
 	    super(props);
 	    this.state = {
 	    	appointment: '',
-	    	bID: ''
+	    	bID: '',
+	    	ownedPets: []
 	    };
 	    this.cancelAppt = this.cancelAppt.bind(this);
     }
@@ -29,6 +30,14 @@ export class OwnerApptDisplay extends React.Component {
 		.catch(function (error) {
 		    console.log(error);
 		});
+    	
+        axios.get('https://tempeturs-group-2.herokuapp.com/api/owner/pets/get')
+    	.then(data => {
+        	this.setState({ownedPets: data.data});
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
     }
 	
     cancelAppt(event) {
@@ -48,13 +57,52 @@ export class OwnerApptDisplay extends React.Component {
 		this.props.history.push('/owner/home');
     }
 	
+    formatHour(hour, minute) {
+    	var value;
+    	var min = this.formatMin(minute);
+    	if(hour > 12){
+    		hour = hour - 12;
+    		value = hour + ':' + min + ' ' + 'PM';
+    	} else if(hour === 0){
+    		hour = 12;
+    		value = hour + ':' + min + ' ' + 'AM';
+    	}else {
+    		value = hour + ':' + min + ' ' + 'AM';
+    	}
+    	
+    	return value;
+    }
+    
+    formatMin(minute){
+    	var value = minute;
+    	if(minute === 0 || minute === 5){
+    		value = '0' + minute;
+    	}
+    	
+    	return value;
+    }
+    
 	render() {
 		return (
 			<div className="container padded">
 				<div>
-					<h5>Id: {this.state.appointment.blockId}</h5>
 					<p>Sitter Username: {this.state.appointment.username}</p>
-					<p>Pets: </p> //getting pets is actually complicated
+					<table>
+					<tr>
+						<td> Start </td>
+						<td> {this.state.appointment.startMonth}/{this.state.appointment.startDay} @ {this.formatHour(this.state.appointment.startHour, this.state.appointment.startMin)}</td>
+					</tr>
+					<tr>
+						<td> End </td>
+						<td> {this.state.appointment.endMonth}/{this.state.appointment.endDay} @ {this.formatHour(this.state.appointment.endHour, this.state.appointment.endMin)}</td>
+					</tr>
+				</table>
+				Pets To be Watched:<br />
+				{this.state.ownedPets.map(e => (
+					<span>
+						<h7>{e.name} ({e.type})</h7>
+					</span>
+                ))}
 				</div>
 				<div>
 					<form onSubmit={this.handleSubmit}>
